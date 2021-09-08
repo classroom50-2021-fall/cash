365c843c362af01417964f1fa5bca89e44b827f8
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2021-09-08 19:12:04 -0400

    Update distribution code

--- cash.c
+++ cash.c
@@ -1,74 +1,66 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int get_cents(void);
-int get_quarters(int cents);
-int get_dimes(int cents);
-int get_nickels(int cents);
-int get_pennies(int cents);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
-    // Ask the user for how many cents they have.
+    // Ask how many cents the customer is owed
     int cents = get_cents();
 
-    // Get the maximum number of quarters that will fit into cents.
-    int quarters = get_quarters(cents);
-  
-    // Update cents by subtracting maximum number of quarters.
-    cents = cents - (quarters * 25);
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
 
-    // Get the maximum number of dimes that will fit into the remaining number of cents.
-    int dimes = get_dimes(cents);
-  
-    // Update cents by subtracting maximum number of dimes.
-    cents = cents - (dimes * 10);
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
 
-    // Get the maximum number of nickels that will fit into the remaining number of cents.
-    int nickels = get_nickels(cents);
-  
-    // Update cents by subtracting maximum number of nickels.
-    cents = cents - (nickels * 5);
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
 
-    // Get the maximum number of pennies that will fit into the remaining number of cents.
-    int pennies = get_pennies(cents);
-  
-    // Update cents by subtracting maximum number of pennies.
-    cents = cents - (pennies * 1);
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
 
     // Sum coins
     int coins = quarters + dimes + nickels + pennies;
 
-    // Print least number of coins to create change
+    // Print total number of coins to give the customer
     printf("%i\n", coins);
 }
 
 int get_cents(void)
 {
-    // TODO: Implement a function to ask the user how many cents they have.
+    // TODO
     return 0;
 }
 
-int get_quarters(int cents)
+int calculate_quarters(int cents)
 {
-    // TODO: Implement a function find how many quarters are in a given number of cents.
+    // TODO
     return 0;
 }
 
-int get_dimes(int cents)
+int calculate_dimes(int cents)
 {
-    // TODO: Implement a function to find how many dimes are in a given number of cents.
+    // TODO
     return 0;
 }
 
-int get_nickels(int cents)
+int calculate_nickels(int cents)
 {
-    // TODO: Implement a function to find how many nickels are in a given number of cents.
+    // TODO
     return 0;
 }
 
-int get_pennies(int cents)
+int calculate_pennies(int cents)
 {
-    // TODO: Implement a function to find how many pennies are in a given number of cents.
+    // TODO
     return 0;
 }
